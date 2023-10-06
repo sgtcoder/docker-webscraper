50ee7b3eac5a9a3e5964dcdacf32c3301e0f5aac
coder <usmccoder@gmail.com>
2023-10-06 11:07:47 -0700

    script update

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -3,7 +3,7 @@
 REGISTRY="sgtcoder"
 PROJECT_NAME="webscraper"
 
-#./deploy.sh --no-cache
+#./deploy-docker.sh --no-cache
 docker build $1 -t $REGISTRY/$PROJECT_NAME . || exit 1
 
 docker push $REGISTRY/$PROJECT_NAME
