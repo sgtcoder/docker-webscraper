a0518ac622acd186d18c4c75f6e27e1e3cb04b4c
coder <usmccoder@gmail.com>
2025-03-16 08:59:11 -0700

    test updates

--- app/test/indexTest.js
+++ app/test/indexTest.js
@@ -2,18 +2,24 @@
 
 var assert = require("assert");
 const axios = require("axios");
-const PORT = 3050;
+const PORT = 3000;
 
 const { app, browserReadyEmitter } = require("../server"); // Use the combined server
 let serverInstance;
+let connections = new Set();
 
 describe("elastic service", function () {
-    this.timeout(10000);
-
     before(function (done) {
         serverInstance = app.listen(PORT, () => {
             // Wait for the browser to be ready
             browserReadyEmitter.once("ready", done);
+        });
+
+        serverInstance.on("connection", (conn) => {
+            connections.add(conn);
+            conn.on("close", () => {
+                connections.delete(conn);
+            });
         });
     });
 
@@ -25,36 +31,52 @@ describe("elastic service", function () {
     });
 
     it("open page and check results", async function () {
-        const response = await axios.get(`http://127.0.0.1:${PORT}/test`);
-        assert.equal(response.data, "<p>Hello World!</p>");
+        const response = await axios.post(`http://127.0.0.1:${PORT}`, {
+            url: `http://127.0.0.1:${PORT}/test`,
+            userAgent: "TestAgent",
+        });
+
+        assert.equal(response.data.data, "<html><head></head><body><p>Hello World!</p></body></html>");
     });
 
     it("open page and check page function results", async function () {
-        const response = await axios.get(`http://127.0.0.1:${PORT}/test`);
-        const result = /<p>(.*?)<\/p>/.exec(response.data)[1];
-        assert.equal(result, "Hello World!");
+        const response = await axios.post(`http://127.0.0.1:${PORT}`, {
+            url: `http://127.0.0.1:${PORT}/test`,
+            userAgent: "TestAgent",
+            pageFunction: 'function($) { return $("p").text() }',
+        });
+
+        assert.equal(response.data.data, "Hello World!");
     });
 
     it("open page and check user agent", async function () {
-        const response = await axios.get(`http://127.0.0.1:${PORT}/agent`, {
-            headers: { "User-Agent": "TestAgent" },
+        const response = await axios.post(`http://127.0.0.1:${PORT}`, {
+            url: `http://127.0.0.1:${PORT}/agent`,
+            userAgent: "TestAgent",
+            pageFunction: 'function($) { return $("p").text() }',
         });
-        const result = /<p>(.*?)<\/p>/.exec(response.data)[1];
-        assert.equal(result, "TestAgent");
+
+        assert.equal(response.data.data, "TestAgent");
     });
 
     it("open page and check custom site", async function () {
-        const response = await axios.get(`https://betterprogramming.pub/how-to-share-a-postgres-socket-between-docker-containers-ad126e430de7`, {
-            headers: { "User-Agent": "TestAgent" },
+        const response = await axios.post(`http://127.0.0.1:${PORT}`, {
+            url: `https://betterprogramming.pub/how-to-share-a-postgres-socket-between-docker-containers-ad126e430de7`,
+            userAgent: "TestAgent",
         });
         // Add any specific checks you need for this page
         assert(response.status === 200);
     });
 
     it("open page and test costco", async function () {
-        const response = await axios.get(`https://www.costco.com/warehouse-locations/thomas-road-az-465.html`, {
-            headers: { "User-Agent": "TestAgent" },
+        const response = await axios.post(`http://127.0.0.1:${PORT}`, {
+            url: `https://www.costco.com/warehouse-locations/thomas-road-az-465.html`,
+            userAgent: "TestAgent",
+            pageFunction: 'function($) { return $("#service-collapse-1").html() }',
         });
+
+        console.log(response.data);
+
         // Add any specific checks you need for this page
         assert(response.status === 200);
     });
